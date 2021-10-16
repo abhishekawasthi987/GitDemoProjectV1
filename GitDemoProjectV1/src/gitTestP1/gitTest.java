56a6da95eea81e6405bcd3e6773da03e7952ad04
320083767 <320083767@YY185999.code1.emi.philips.com>
2021-10-16 12:21:16 +0530

    First Commit

---
+++ GitDemoProjectV1/src/gitTestP1/gitTest.java
@@ -0,0 +1,19 @@
+package gitTestP1;
+
+public class gitTest {
+
+	public static void main(String[] args) {
+		
+int a = 10;
+		
+		if(a==10)
+			System.out.println("True");
+		else {
+		System.out.println("False");
+
+
+	}
+
+}
+
+}
